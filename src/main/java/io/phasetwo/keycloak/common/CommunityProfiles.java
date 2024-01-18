58b093ced335095367fbc33a9dd3aa127e0d50b9
Garth <244253+xgp@users.noreply.github.com>
2024-01-18 14:42:30 +0100

    merged poms. added back java prop for enabling.

--- src/main/java/io/phasetwo/keycloak/common/CommunityProfiles.java
+++ src/main/java/io/phasetwo/keycloak/common/CommunityProfiles.java
@@ -2,11 +2,13 @@ package io.phasetwo.keycloak.common;
 
 public class CommunityProfiles {
   private static final String ENV_JPA_CACHE_ENABLED = "KC_COMMUNITY_JPA_CACHE_ENABLED";
+  private static final String PROP_JPA_CACHE_ENABLED = "kc.community.jpa.cache.enabled";
 
   private static final boolean isJpaCacheEnabled;
   
   static {
-    isJpaCacheEnabled = Boolean.parseBoolean(System.getenv(ENV_JPA_CACHE_ENABLED));
+    isJpaCacheEnabled = Boolean.parseBoolean(System.getenv(ENV_JPA_CACHE_ENABLED)) ||
+                        Boolean.parseBoolean(System.getProperty(PROP_JPA_CACHE_ENABLED));
   }
   
   public static boolean isJpaCacheEnabled() {
